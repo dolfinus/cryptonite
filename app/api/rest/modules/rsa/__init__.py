3a2d86a929a2881e34510fc90efdaff9b5be11dc
dolfinus <martinov_m_s_@mail.ru>
2018-06-28 01:30:57 +0300

    Allow RSA to encrypt text message

--- app/api/rest/modules/rsa/__init__.py
+++ app/api/rest/modules/rsa/__init__.py
@@ -19,28 +19,38 @@ def nod(a,b):
 
 class RSA:
 
-  def __init__(self, mod):
-    print(mod)
+  def __init__(self, mod, e):
     self.mod = int(mod)
+    self.e   = int(e)
 
+  @staticmethod
   def config():
-    with open(os.path.dirname(__file__)+'/config.yml','r') as cfg:
+    with open(os.path.dirname(__file__)+'/config.yml', 'r') as cfg:
       return yaml.load(cfg)
 
-  def inv(self, e):
+  def inv(self):
     phi_ = phi(self.mod)
     for d in range(1, phi_):
-      if (d * e) % phi_ == 1:
+      if (d * self.e) % phi_ == 1:
         return d
 
-  def encrypt(self, nums, e):
-    e = int(e)
+  def encrypt(self, nums, e=None):
     result = []
+    if not e:
+      e = self.e
+    if isinstance(nums, str):
+      if not nums.isdigit():
+        nums = bytearray(nums, 'UTF-8')
     for num in nums:
+      num = int(num)
       result.append(pow(num, e, self.mod))
+    #if isinstance(nums, str):
+    #  result = ''.join(map(chr, result))
     return result
 
-  def decrypt(self, nums, d=None, e=None):
-    if e:
-      d = self.inv(e)
+  def decrypt(self, nums, d=None):
+    if not d:
+      d = self.inv()
+    else:
+      d = int(d)
     return self.encrypt(nums, d)
